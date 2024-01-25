be13a82454230ecc3d73989a6736cbdc8a9db058
IndiartoAji13 <indiartoaji13@gmail.com>
2024-01-25 19:47:39 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,74 @@
-import React, { useState } from "react";
-import { fetchWeather } from './api/fetchWeather'
-import './App.css'
+import * as React from 'react';
+import { useState } from "react";
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { Header } from './components/Header';
+import { BodyWeather } from './components/BodyWeather';
+import { BodyForecast } from './components/BodyForecast';
+
+
+
 
 const App = () => {
    const [query, setQuery] = useState('');
    const [weather, setWeather] = useState('');
+   const [airQuality, setAirQuality] = useState('');
+   const [areaLat, setAreaLat] = useState('')
+   const [areaLon, setAreaLon] = useState('')
+   const [toggleDarkMode, setToggleDarkMode] = useState(true);
 
-   const search = async (e) => {
-      if (e.key === 'Enter') {
-         const data = await fetchWeather(query)
-         setWeather(data)
-         setQuery('');
+   const toggleDarkTheme = () => {
+      setToggleDarkMode(!toggleDarkMode);
+   };
+
+   const darkTheme = createTheme({
+      palette: {
+         mode: toggleDarkMode ? 'dark' : 'light',
+         primary: {
+            main: '#90caf9',
+         },
+         secondary: {
+            main: '#131052',
+         },
       }
-   }
+   });
 
 
    return (
-      <div className="main-container">
-         <input
-            type="text"
-            className="search"
-            placeholder="Search..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            onKeyPress={search}
+      <ThemeProvider theme={darkTheme}>
+         <CssBaseline />
+         <Header
+            query={query}
+            setQuery={setQuery}
+            setWeather={setWeather}
+            setAirQuality={setAirQuality}
+            toggleDarkMode={toggleDarkMode}
+            toggleDarkTheme={toggleDarkTheme}
+         />
+         <br />
+         <BodyWeather
+            weather={weather}
+            airQuality={airQuality}
          />
 
-         {weather.main && (
-            <div className="city">
-               <h2 className="city-name">
-                  <span>{weather.name}</span>
-                  <sup>{weather.sys.country}</sup>
-               </h2>
-               <div className="city-temp">
-                  {Math.round(weather.main.temp)}
-                  <sup>&deg;C</sup>
-               </div>
-               <div className="info">
-                  <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-                  <p>{weather.weather[0].description}</p>
-               </div>
-            </div>
-         )}
-      </div>
+         {/* <div className="city">
+            <h1>Air Quality pm10</h1>
+            <h1 >
+               {airQuality.hourly.pm10[0]}
+               <sup>μg/m³</sup>
+            </h1>
+
+            <h1>Air Quality pm2.5</h1>
+            <h1 >
+               {airQuality.hourly.pm2_5[0]}
+               <sup>μg/m³</sup>
+            </h1>
+         </div> */}
+
+         <BodyForecast></BodyForecast>
+      </ThemeProvider>
+
 
    )
 }
